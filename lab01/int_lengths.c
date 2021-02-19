83462867947d679d40b24a8b8a60e6bf72eee0af
Miles Alasdair Martin <miles.martin@maine.edu>
2021-02-18 20:33:49 -0500

    Can't believe we forgot to put everything into a main function, wow

--- lab01/int_lengths.c
+++ lab01/int_lengths.c
@@ -8,21 +8,30 @@
 #include <stdio.h>                                              // adds printf()
 #include <limits.h>                                             // adds the defines for max length of these variables
 
-printf("A float is %d bits long in decimal", float);            // these are the lengths of different variable types
-printf("A double is %d bits long in decimal", double);
-printf("A char is %x bits long in hex", char);          
-printf("A short is %x bits long in hex", short);
-printf("An int is %x bits long in hex", int);
-printf("A long is %x bits long in hex", long);
-printf("A float is %x bits long in hex", float);
-printf("A double is %x bits long in hex", double);
+void main() {
 
-printf("Decimal size of a signed int is %d", INT_MAX);          // decimal size of a signed int
-printf("Decimal size of an unsigned int is %d", UINT_MAX);      // decimal size of an unsigned int
-printf("Decimal size of a signed long int is %d", LONG_MAX);    // decimal size of a signed long int
-printf("Decimal size of an unsigned long int is %d", ULONG_MAX);// decimal size of an unsigned long int
+    printf("A char is %d bits long in decimal\n", sizeof(char));          
+    printf("A short is %d bits long in decimal\n", sizeof(short));
+    printf("An int is %d bits long in decimal\n", sizeof(int));
+    printf("A long is %d bits long in decimal\n", sizeof(long));
+    printf("A float is %d bits long in decimal\n", sizeof(float));            // these are the lengths of different variable types
+    printf("A double is %d bits long in decimal\n", sizeof(double));
 
-printf("Hex size of a signed int is %x", INT_MAX);              // hex size of a signed int
-printf("Hex size of an unsigned int is %x", UINT_MAX);          // hex size of an unsigned int
-printf("Hex size of a signed long int is %x", LONG_MAX);        // hex size of a signed long int
-printf("Hex size of an unsigned long int is %x", ULONG_MAX);    // hex size of an unsigned long int
+    printf("A char is %x bits long in hex\n", sizeof(char));          
+    printf("A short is %x bits long in hex\n", sizeof(short));
+    printf("An int is %x bits long in hex\n", sizeof(int));
+    printf("A long is %x bits long in hex\n", sizeof(long));
+    printf("A float is %x bits long in hex\n", sizeof(float));
+    printf("A double is %x bits long in hex\n", sizeof(double));
+
+    printf("Decimal size of a signed int is %d\n", INT_MAX);          // decimal size of a signed int
+    printf("Decimal size of an unsigned int is %d\n", UINT_MAX);      // decimal size of an unsigned int
+    printf("Decimal size of a signed long int is %ld\n", LONG_MAX);    // decimal size of a signed long int
+    printf("Decimal size of an unsigned long int is %ld\n", ULONG_MAX);// decimal size of an unsigned long int
+
+    printf("Hex size of a signed int is %x\n", INT_MAX);              // hex size of a signed int
+    printf("Hex size of an unsigned int is %x\n", UINT_MAX);          // hex size of an unsigned int
+    printf("Hex size of a signed long int is %x\n", LONG_MAX);        // hex size of a signed long int
+    printf("Hex size of an unsigned long int is %x\n", ULONG_MAX);    // hex size of an unsigned long int
+
+}        
