85d38f481d555acf8f4c98a1e41012bc9e3c6e28
Miles Alasdair Martin <miles.martin@maine.edu>
2021-02-19 10:18:19 -0500

    Fixed representation issues of uints and ulongs

---
+++ lab01/code_fragments/int_lengths_part2.c
@@ -0,0 +1,12 @@
+printf("A char is %d bits long in decimal", char);          
+printf("A short is %d bits long in decimal", short);
+printf("An int is %d bits long in decimal", int);
+printf("A long is %d bits long in decimal", long);
+printf("A float is %d bits long in decimal", float);
+printf("A double is %d bits long in decimal", double);
+printf("A char is %x bits long in hex", char);          
+printf("A short is %x bits long in hex", short);
+printf("An int is %x bits long in hex", int);
+printf("A long is %x bits long in hex", long);
+printf("A float is %x bits long in hex", float);
+printf("A double is %x bits long in hex", double);
