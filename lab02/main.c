aa937560ec2c1ccd0774585014b3036f5ad9c2e1
Miles Martin <miles.martin@maine.edu>
2021-02-26 17:38:08 +0000

    Still troubleshooting arrays

--- lab02/main.c
+++ lab02/main.c
@@ -17,12 +17,13 @@
 // not really sure if this is the right approach.
 // looking for value to pass to 2nd arg. of pwm (the period)
 
-void pwm(float duty_cycle_pct, float period_ms, int *pins[]);
+void pwm(float duty_cycle_pct, float period_ms, int pins[]);
 
 const int pin_row[] = {22, 26, 23, 27, 24, 28, 29, 25}; 	// WiringPi pin numbers
 								// lined up on breadboard 
 
 const int num_pins = sizeof(pin_row)/sizeof(pin_row[0]);	// Max times to loop
+printf("num_pins equals %d\n",num_pins);
 
 int main (int argc, char *argv[]) {
 	
@@ -74,7 +75,7 @@ int main (int argc, char *argv[]) {
 
 	
 	while(1) {
-		pwm(brightness_pct, 25, pin_row[]);
+		pwm(brightness_pct, 25, pin_row[num_pins]);
 	}
 }
 
@@ -84,11 +85,11 @@ void pwm(float duty_cycle_pct, float period_ms, int pins[]) {
 
 	int i;
 
-	int num_pins = sizeof(pins)/sizeof(pins[0]);
+	int num_of_pins = sizeof(pins)/sizeof(pins[0]);
 
 	while(1) {
 	
-		for(i = 0; i < num_pins; i++) {
+		for(i = 0; i < num_of_pins; i++) {
 		
 			digitalWrite(pins[i], HIGH);		// set pins; turn on lights
 	
@@ -96,7 +97,7 @@ void pwm(float duty_cycle_pct, float period_ms, int pins[]) {
 
 		delay(time_high);						// keep high appropriately long
 
-		for(i = 0; i < num_pins; i++) {
+		for(i = 0; i < num_of_pins; i++) {
 		
 			digitalWrite(pins[i], LOW);		// clear pins; turn off lights
 	
