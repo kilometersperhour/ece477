3b7f7879a83f0ee86c2c19390e0408bccadc826c
Miles Martin <miles.martin@maine.edu>
2021-02-26 20:58:43 +0000

    Doesn't work but we're so close...

--- lab02/main.c
+++ lab02/main.c
@@ -10,8 +10,9 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 #include <wiringPi.h>
-
 
 #define BLINK_PERIOD_MS 10
 
@@ -24,13 +25,64 @@ const int num_pins = sizeof(pin_row)/sizeof(pin_row[0]);	// Max times to loop
 
 int main (int argc, char *argv[]) {
 
+	if ((2 > argc) || (2 < argc)) {
+		
+		printf("\nPlease enter exactly two arguments.\n");
+		return 0;
+
+	}
+
+	int brightness;
+
+	int i = 0;
+	for (i = 0; i < strlen(argv[1]); i++) {
+
+		argv[1][i] = toupper(argv[1][i]);
+	
+	}
+	if ( { // begin conditions for hex input
+	('0' == argv[1][0]) && 
+	('x' == argv[1][1]) && 
+	((('0' <= argv[1][2]) && ('9' >= argv[1][2])) || (('A' <= argv[1][2]) && ('F' >= argv[1][2])) && 
+	((('0' <= argv[1][2]) && ('9' >= argv[1][2])) || (('A' <= argv[1][2]) && ('F' >= argv[1][2])) &&  
+	} ) // end conditions 
+	{ // begin inner statement
+	
+		brightness = strtol(argv[1], NULL, 16);
+		printf("hex\n");
+
+	} else if ( { // begin conditions for octal input
+		('0' == argv[1][0]) && 
+		(('0' <= argv[1][1]) || ('3' >= argv[1][1])) && 
+		(('0' <= argv[1][2]) || ('7' >= argv[1][2])) && 
+		(('0' <= argv[1][3]) || ('7' >= argv[1][3])) 
+	} ) 	// end conditions
+	{	// begin inner statement
+	
+		brightness = strtol(argv[1], NULL, 8);
+		printf("octal\n");
+
+	} else if ( { // begin conditions for decimal input
+		('0' == argv[1][0]) && 
+		(('0' <= argv[1][1]) || ('9' >= argv[1][1])) && 
+		(('0' <= argv[1][2]) || ('9' >= argv[1][2])) && 
+		(('0' <= argv[1][3]) || ('9' >= argv[1][3])) 
+	} ) 	// end conditions
+	{	// begin inner statement	// Hopefully decimal
+		
+		brightness = strtol(argv[1], NULL, 10);
+		printf("decimal\n");
+
+	} else {
+		printf("Please correct the format of your input.\n");
+		printf("Valid input examples; \n dec: 'lab2a 255', \n oct 'lab2a 0377', \n hex 'lab2a 0xff'");
+	}
 	
 
-	int brightness = atoi(argv[1]);
 
 	if ((255 < brightness)||(0 > brightness)) {
 
-		printf("Error: please pass an argument between 0 and 255 in decimal.\n");
+		printf("\nError: please pass an argument between 0 and 255 in decimal.\n");
 		return 0;
 
 	} 
@@ -49,8 +101,8 @@ int main (int argc, char *argv[]) {
 		} else {
 
 		//	printf("Child process %d still alive\n", child);
-			printf("The program has been forked to the background.");
-			printf("Before running lab2a again, run 'kill %d'",child);
+			printf("\nThe program has been forked to the background.\n");
+			printf("Before running lab2a again, run 'kill %d'\n",child);
 
 			return 0;
 
