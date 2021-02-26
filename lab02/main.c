4d591402fe1e06ec0010e4d70175f6e7299ecbe7
Miles Alasdair Martin <miles.martin@maine.edu>
2021-02-26 11:30:01 -0500

    Moved PWM function to main.c; was really struggling with header files

--- lab02/main.c
+++ lab02/main.c
@@ -1,12 +1,15 @@
-#include "pwm_uniform.h"
+//#include "pwm_uniform.h"
 #include <sys/types.h> // is this needed for fork()?
 #include <unistd.h>
 #include <stdio.h>
+#include <wiringPi.h>
 
 
-#define VISIBLE_THRESHHOLD (float) 25/1000 // 25 Hz / 1000 ms ... 
+// #define VISIBLE_THRESHHOLD (float) 25/1000 // 25 Hz / 1000 ms ... 
 // not really sure if this is the right approach.
 // looking for value to pass to 2nd arg. of pwm (the period)
+
+void pwm(float duty_cycle_pct, float period_ms, int pins[]);
 
 const int pin_row[] = {22, 26, 23, 27, 24, 28, 29, 25}; 	// WiringPi pin numbers
 								// lined up on breadboard 
@@ -18,15 +21,15 @@ int main (int argc, char *argv[]) {
 	
 	int brightness = atoi(argv[1]);
 
-	if (255 <= brightness) {
+	if (255 < brightness) {
 
 		printf("Error: please pass an argument between 0 and 255 in dec, hex, or octal.\n");
 		return 0;
 
-	} else {
+	} else { // if input valid
 
+		int child = fork();
 		int parent = getppid();
-		int child = getpid();
 
 		printf("Child process: %d\n",child);
 		printf("Parent process: %d\n",parent);
@@ -44,14 +47,80 @@ int main (int argc, char *argv[]) {
 
 	}
 
-	float brightness_pct = brightness/255; 
+	float brightness_pct = brightness/255; // between 0 and 1
 	printf("Your desired brightness: %f\n",brightness_pct);
 	
 	int i;
 	
 	wiringPiSetup();					// init
 	
-	for(i = 0; i < num_pins; i++) {				
+	while(1) {
+		pwm(brightness_pct, 25, pin_row);
+	}
+}
+
+void pwm(float duty_cycle_pct, float period_ms, int pins[]) {
+		
+	float time_high = duty_cycle_pct * period_ms;
+
+	int i;
+
+	int num_pins = sizeof(pins)/sizeof(pins[0]);
+
+	while(1) {
+	
+		for(i = 0; i < num_pins; i++) {
+		
+			digitalWrite(pins[i], HIGH);		// set pins; turn on lights
+	
+		}
+
+		delay(time_high);						// keep high appropriately long
+
+		for(i = 0; i < num_pins; i++) {
+		
+			digitalWrite(pins[i], LOW);		// clear pins; turn off lights
+	
+		}
+
+		delay(period_ms - time_high);			// keep dark appropriately long
+	}
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*	for(i = 0; i < num_pins; i++) {				
 		
 		pinMode(pin_row[i], OUTPUT);			// set pin_row pins to OUTPUT
 	
@@ -74,5 +143,5 @@ int main (int argc, char *argv[]) {
 	
 		}
 	//}
-	return 0;
+*/	return 0;
 }
