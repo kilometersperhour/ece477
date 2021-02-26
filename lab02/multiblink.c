a37406af3a7049d3012866ee7b40dda5bb84d4bd
Miles Martin <miles.martin@maine.edu>
2021-02-26 01:44:03 +0000

    Initialized the pins using a for loop so that the toggling works

--- lab02/multiblink.c
+++ lab02/multiblink.c
@@ -9,20 +9,27 @@
 int pin_row[] = {22, 26, 23, 27, 24, 28, 29, 25};
 
 int main (void) {
+	int i;
+	
 	wiringPiSetup();
 	
-	pinMode(0, OUTPUT);
+	for(i = 0; i < 8; i++) {
+		
+		pinMode(pin_row[i], OUTPUT);
 	
+	}
+
+
 	while(1) {
 	
-		for(int i = 0; i < 8; i++) {
+		for(i = 0; i < 8; i++) {
 		
 			digitalWrite(pin_row[i], HIGH);
 			delay(500);
 	
 		}
 	
-		for(int i = 0; i < 8; i++) {
+		for(i = 0; i < 8; i++) {
 		
 			digitalWrite(pin_row[i], LOW);
 			delay(500);
