69f756d0394a29b029af610e8637a26946e20f1c
Miles Alasdair Martin <miles.martin@maine.edu>
2021-02-25 23:00:58 -0500

    created new header file and finished code for uniform lighting

--- lab02/pwm_uniform.c
+++ lab02/pwm_uniform.c
@@ -1,26 +1,30 @@
-#include <wiringPi.h>
-#include "pwm.h"
+#include "pwm_uniform.h"
 
-extern void pwm(int duty_cycle, int period, int pins[]) {
+extern void pwm(float duty_cycle_pct, float period_ms, int pins[]) {
 	
+	float time_high = duty_cycle_pct * period_ms;
+
 	int i;
-	num_pins = sizeof(pins)/sizeof(pins[0]);
+
+	int num_pins = sizeof(pins)/sizeof(pins[0]);
 
 	while(1) {
 	
 		for(i = 0; i < num_pins; i++) {
 		
 			digitalWrite(pin_row[i], HIGH);		// set pins; turn on lights
-			delay(500);
 	
 		}
-	
+
+		delay(time_high);						// keep high appropriately long
+
 		for(i = 0; i < num_pins; i++) {
 		
 			digitalWrite(pin_row[i], LOW);		// clear pins; turn off lights
-			delay(500);
 	
 		}
+
+		delay(period_ms - time_high);			// keep dark appropriately long
 	}
 	return 0;
 }
---
+++ lab02/pwm_uniform.h
@@ -0,0 +1,3 @@
+#include <wiringPi.h>
+
+extern void pwm(float duty_cycle_pct, float period_ms, int pins[]);
