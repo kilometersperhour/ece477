a5696b69de20e4907ad3c5f9e05bea8ac2512699
Miles Martin <miles.martin@maine.edu>
2021-03-11 23:06:11 -0500

    main.c compiles, but doesn't appear to do any of the shifting it should

--- lab4/main.c
+++ lab4/main.c
@@ -12,7 +12,7 @@
 #include <ctype.h>
 #include <string.h>
 #include <wiringPi.h>
-#include "shift.h"
+//#include "shift.h"
 
 #define LS 1<<0  //least significant position, rightmost LED lit up 
 #define MS 1<<7  //most significant position, leftmost LED lit up
@@ -33,74 +33,75 @@ const int input_pins[2] = {
 1,    // GPIO 18, button B
 };
 
-const int num_pins = sizeof(pin_row)/sizeof(pin_row[0]);  // Max times to loop
-
+const int num_inputs = sizeof(input_pins)/sizeof(input_pins[0]);  // Max times to loop
+const int num_outputs = sizeof(output_pins)/sizeof(output_pins[0]);  // Max times to loop
 
 int main (int argc, char *argv[]) {
+	
+	// Miles Martin
 
 	int i;
-	uint8_t state = 1;
+	unsigned int state = 1;
 	unsigned int current_button[2], last_button[2];
 	unsigned int execute; 
-	unsigned int delay = 1024;
 	unsigned int wait_time = 1024;
 	signed int direction = 1;    // oscillates between 1 and -1
                                      // (getting smaller vs. getting bigger)
 	
 	wiringPiSetup();             // pin init, probably?	
 	
-	for(i = 0; i < num_pins; i++) { 
+	for(i = 0; i < num_outputs; i++) { 
 		pinMode(output_pins[i], OUTPUT);
 	}
 
-	for(i = 0; i < 2; i++) {
+	for(i = 0; i < num_inputs; i++) {
 		pinMode(input_pins[i], INPUT);
 		pullUpDnControl (input_pins[i], PUD_UP);  // enable pull up resistors
 	}
 
-	// Travis' section??
-
-	
-	for(i=0; i<2; i++){
-		last_button[i] = current_button[i];
-		current_button[i] = digitalRead(input_pins[i]);
-	}
-		if 	((current_button[0] == last_button[0]) && 
-			(current_button[1] == last_button[1])) {
-			execute = 0;
-		} else {
-			execute = 1;
-		}
-
-
-	if (execute) {
-	
-		if (current_button[0] && current_button[1]) {
-			return 0; // end program
-		} else if (current_button[0]) {
-			if (wait_time == 32) {
-				direction *= -1; // reverse direction
-			} else {
-				wait_time /= 2; // increase refresh rate
-			}
-		} else if (current_button[1]) {
-			if (wait_time == 1024) {
-				direction *= -1; // reverse direction
-			} else {
-				delay *= 2; // decrease refresh rate
-			}
-		}
-	} 
-
-	// Jesse's super succinct code :D
-
+	// Travis Nickerson
 	while(1) {
 
+		for(i=0; i<2; i++){
+			last_button[i] = current_button[i];
+			current_button[i] = digitalRead(input_pins[i]);
+		}
+			if 	((current_button[0] == last_button[0]) && 
+				(current_button[1] == last_button[1])) {
+				execute = 0;
+			} else {
+				execute = 1;
+			}
+	
+	
+		// Miles Martin
+	
+		if (execute) {
+		
+			if (current_button[0] && current_button[1]) {
+				return 0; // end program
+			} else if (current_button[0]) {
+				if (wait_time == 32) {
+					direction *= -1; // reverse direction
+				} else {
+					wait_time /= 2; // increase refresh rate
+				}
+			} else if (current_button[1]) {
+				if (wait_time == 1024) {
+					direction *= -1; // reverse direction
+				} else {
+					wait_time *= 2; // decrease refresh rate
+				}
+			}
+		} 
+	
+		// Jesse Perkins
+	
 		if(0 < direction) {
 			//if the direction is 1 (going right to left) and 
 			//the current state isn't LS, shift left LED by 
 			//one. If it is LS, wrap around to MS
-	        	(state == LS)? state = MS: state << 1; 
+		       	(state == LS)? state = MS: state << 1; 
 		}
 		else {
 			//if the direction is not 1 (going left to right)  
@@ -108,5 +109,12 @@ int main (int argc, char *argv[]) {
 			//one. If it is MS, wrap around to LS
 			(state == MS)? state = LS: state >> 1;
 		}
+		
+		// Miles Martin
+		for (i = 0; i < 8; i++) {
+			printf("%d",(state>>(7-i)&1));
+		}
+		printf("\n");
+	
 	}
 }
