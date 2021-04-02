cf47a7ae836ae40e3f7f9e45ae92e4a816866f2c
Miles Martin <miles.martin@maine.edu>
2021-04-02 15:07:40 -0400

    Pushing code for lab partners

--- lab5/picode.c
+++ lab5/picode.c
@@ -3,18 +3,19 @@
 #include <stdio.h>                                          //included to debug sample collection
 
 #define PIN 0                                            //GPIO 17 = wiring pi 0
+#define DURATION 1000
 
 struct timeval timeofday;
 
 __uint8_t offset;
 __uint8_t direction;
 
-unsigned long long samples_time[1000];                                  //1000 samples per second, for a second
-__uint8_t samples_value[1000];
+unsigned long long samples_time[DURATION];                                  //1000 samples per second, for a second
+__uint8_t samples_value[DURATION];
 
 void clock_setter(void);
 
-unsigned long long time_prev, time_now, start_time;
+unsigned long long start_time, time_now, time_prev;
 
 main() {
     clock_setter();
@@ -24,9 +25,12 @@ main() {
 
 void clock_setter(void) {
     char string[81];
-    int i;
-    double periodsum;
-    double frequency;
+    int i,j;
+    int edges[DURATION-2];
+    int periods[DURATION-3];
+    int calibration = 0;
+    unsigned long long periodsum;
+    double frequency, period;
 
     while(1) { 
         
@@ -36,16 +40,15 @@ void clock_setter(void) {
 	
 	gettimeofday(&timeofday, NULL);
         
-	start_time = (unsigned long long)(timeofday.tv_sec) * 1000 + (unsigned long long)(timeofday.tv_usec) / 1000;
-	time_now = start_time;
-	time_prev = start_time;
+	start_time = ((unsigned long long)(timeofday.tv_sec) * 1000 + (unsigned long long)(timeofday.tv_usec) / 1000);
+	time_now = 0;
+	time_prev = 0;
 
-        while(time_now < start_time + 1000) {                   //while time between 0-1s, sample
+        while(time_now < DURATION) {                   //while time between 0-1s, sample
             gettimeofday(&timeofday, NULL);                     //constantly check time
-            time_now = (unsigned long long)(timeofday.tv_sec) * 1000 + (unsigned long long)(timeofday.tv_usec) / 1000; 
+            time_now = ((unsigned long long)(timeofday.tv_sec) * 1000 + (unsigned long long)(timeofday.tv_usec) / 1000)-start_time; 
 
             if(time_now >= time_prev + 1) {                     //if a millisecond has passed
-		
 		samples_time[i] = time_now;                     // take a snapshot
 		samples_value[i] = digitalRead(PIN);
 	
@@ -54,19 +57,53 @@ void clock_setter(void) {
                 time_prev = time_now;                           //remember last sample time
             }
 	
-            periodsum += (double)samples_time;                             // Sum all the periods together
+            periodsum += samples_time[i-1];                             // Sum all the periods together
        
        	}
         
-	printf("%d", samples_time);
+	for (i = 0; i < DURATION - 1; i++) {
+	    if (samples_value[i] != samples_value[i+1]) {
+	        edges[j] = i; // list of all rising/falling edges' indices in sample_value
+		j++;
+		printf("I made it! j is %d\n", j);
+	    }
+	}
+	
+        for (i = 0; i < j-1; i++) {
+	    printf("i is %d\n", i);
+	    periods[edges[i]] = ((samples_time[i+1] - samples_time[i]) / 2.) + samples_time[i];
+	}
+
+	for (i = 0; i < j-2; i++) {
+	    periodsum += periods[edges[i]];
+	}
+
+	period = periodsum / (j-2);
+
+	frequency = 1. / period;
+
+	if (frequency < 100) {
+	
+            calibration--;
+
+	} else if (frequency > 100) {
+	
+            calibration++;
+
+	}
+
+        printf("%d", calibration);
+
+	/*
+	printf("%llu\n", samples_time[i]);
 	
 	frequency = (1/(periodsum/(i))); 			// Computes frequency from the averaged period over the 1000ms sample time
 	printf("i = %d\n", (i));
-	printf("periodsum = %f\n", periodsum);
-	printf("periodsum/(i) = %f\n", (periodsum/(i)));
-	printf("1/(periodsum/(i)) = %f\n", (1/(periodsum/(i))));
-	printf("f is %f and i is %d\n\n", frequency, i);
-
+	printf("periodsum = %llu\n", periodsum);
+	printf("(double)periodsum/(i) = %lf\n", ((double)periodsum/(i)));
+	printf("1/((double)periodsum/(i)) = %lf\n", (1/((double)periodsum/(i))));
+	printf("f is %lfE9 and i is %d\n\n", frequency, i);
+        */
         sprintf(string, "sudo avrdude -C ~/avrdude_gpio.conf -c pi_1 -p atmega88p -U eeprom:w:%x,%x:m", offset, direction);
        // system(string);
     }
