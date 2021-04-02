c4e8d8d16fd91ed0cea9a0b8665fea72fe34ce39
Miles Martin <miles.martin@maine.edu>
2021-04-02 13:16:04 -0400

    Removed struct and struct helper function. Replaced with standalone variables; need to learn structs better

--- lab5/picode.c
+++ lab5/picode.c
@@ -4,19 +4,14 @@
 
 #define PIN 0                                            //GPIO 17 = wiring pi 0
 
-struct Pair{                                                //keep data points grouped
-    int time;
-    int value;
-};
-
 struct timeval timeofday;
 
 __uint8_t offset;
 __uint8_t direction;
 
-struct Pair samples[1000];                                  //1000 samples per second, for a second
+unsigned long long samples_time[1000];                                  //1000 samples per second, for a second
+__uint8_t samples_value[1000];
 
-struct Pair sample(int t, int v, struct Pair pair); 
 void clock_setter(void);
 
 unsigned long long time_prev, time_now, start_time;
@@ -30,8 +25,8 @@ main() {
 void clock_setter(void) {
     char string[81];
     int i;
-    float periodsum;
-    float frequency;
+    double periodsum;
+    double frequency;
 
     while(1) { 
         
@@ -50,25 +45,29 @@ void clock_setter(void) {
             time_now = (unsigned long long)(timeofday.tv_sec) * 1000 + (unsigned long long)(timeofday.tv_usec) / 1000; 
 
             if(time_now >= time_prev + 1) {                     //if a millisecond has passed
-                sample(time_now, digitalRead(PIN), samples[i]);             //takes a snapshot
+		
+		samples_time[i] = time_now;                     // take a snapshot
+		samples_value[i] = digitalRead(PIN);
+		
 		i++;
+		//printf("i is %d\n",i);
                 time_prev = time_now;                           //remember last sample time
             }
 	
-            periodsum += samples[i].time;                             // Sum all the periods together
+            periodsum += (double)samples_time;                             // Sum all the periods together
        
        	}
-
-        frequency = (1/(periodsum/i)); 			// Computes frequency from the averaged period over the 1000ms sample time
-	printf("%f\n", frequency);
+        
+	printf("%d", samples_time);
+	
+	frequency = (1/(periodsum/(i))); 			// Computes frequency from the averaged period over the 1000ms sample time
+	printf("i = %d\n", (i));
+	printf("periodsum = %f\n", periodsum);
+	printf("periodsum/(i) = %f\n", (periodsum/(i)));
+	printf("1/(periodsum/(i)) = %f\n", (1/(periodsum/(i))));
+	printf("f is %f and i is %d\n\n", frequency, i);
 
         sprintf(string, "sudo avrdude -C ~/avrdude_gpio.conf -c pi_1 -p atmega88p -U eeprom:w:%x,%x:m", offset, direction);
        // system(string);
     }
-}
-
-struct Pair sample(int t, int v, struct Pair pair) {
-    pair.time = t;
-    pair.value = v;
-    return pair;
 }
