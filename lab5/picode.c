b543352f48cb40074b6546806eb58fe9891ac858
Miles Martin <miles.martin@maine.edu>
2021-04-02 12:40:31 -0400

    The code compiles for the first time and will be tested with an oscilloscope and wave generator; known bug: prints inf not frequency

--- lab5/picode.c
+++ lab5/picode.c
@@ -13,8 +13,12 @@ struct timeval timeofday;
 
 __uint8_t offset;
 __uint8_t direction;
+
 struct Pair samples[1000];                                  //1000 samples per second, for a second
+
 struct Pair sample(int t, int v, struct Pair pair); 
+void clock_setter(void);
+
 unsigned long long time_prev, time_now, start_time;
 
 main() {
@@ -25,10 +29,21 @@ main() {
 
 void clock_setter(void) {
     char string[81];
-    int i = 0;
+    int i;
+    float periodsum;
+    float frequency;
+
     while(1) { 
-        gettimeofday(&timeofday, NULL);
-        time_prev, time_now, start_time = (unsigned long long)(timeofday.tv_sec) * 1000 + (unsigned long long)(timeofday.tv_usec) / 1000;
+        
+	i = 0;
+	periodsum = 0;
+	frequency = 0;
+	
+	gettimeofday(&timeofday, NULL);
+        
+	start_time = (unsigned long long)(timeofday.tv_sec) * 1000 + (unsigned long long)(timeofday.tv_usec) / 1000;
+	time_now = start_time;
+	time_prev = start_time;
 
         while(time_now < start_time + 1000) {                   //while time between 0-1s, sample
             gettimeofday(&timeofday, NULL);                     //constantly check time
@@ -40,12 +55,12 @@ void clock_setter(void) {
                 time_prev = time_now;                           //remember last sample time
             }
 	
-	   int periodsum += periodsum+samples[i]		// Sum all the periods together
+            periodsum += samples[i].time;                             // Sum all the periods together
        
        	}
 
-       int frequency = (1/(periodsum/i)); 			// Computes frequency from the averaged period over the 1000ms sample time
-	printf(frequency);
+        frequency = (1/(periodsum/i)); 			// Computes frequency from the averaged period over the 1000ms sample time
+	printf("%f\n", frequency);
 
         sprintf(string, "sudo avrdude -C ~/avrdude_gpio.conf -c pi_1 -p atmega88p -U eeprom:w:%x,%x:m", offset, direction);
        // system(string);
