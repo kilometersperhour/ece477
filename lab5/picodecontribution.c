fac20d366e4a8ea12f96e5d4ddd56146a42d189c
Jesse Perkins <jnperkins13@gmail.com>
2021-04-02 00:46:51 -0400

    co-developed a program which samples a signal at 1kHz with the intention to calibrate it; the code to reprogram the avr is also included.

--- lab5/picodecontribution.c
+++ lab5/picodecontribution.c
@@ -1,28 +1,54 @@
+#include <sys/time.h>
+#include <wiringPi.h>
+#include <stdio.h>                                          //included to debug sample collection
+
+#define PIN 0                                            //GPIO 17 = wiring pi 0
+
 struct Pair{                                                //keep data points grouped
     int time;
     int value;
 };
 
+struct timeval timeofday;
+
+__uint8_t offset;
+__uint8_t direction;
 struct Pair samples[1000];                                  //1000 samples per second, for a second
 struct Pair sample(int t, int v, struct Pair pair); 
+unsigned long long time_prev, time_now, start_time;
 
-while(1) { 
-    time_prev, time_now, start_time = getCurrentTime;
+main() {
+    clock_setter();
 
-    while(time_now < start_time + 1000ms) {                 //while time between 0-1s, sample
-        time_now = getCurrentTime;                          //constantly check time
 
-        if(time_now >= time_prev + 1ms) {                   //if a millisecond has passed
-            sample(time_now, digitalRead(PIN));             //takes a snapshot
-            time_prev = time_now;                           //remember last sample time
+}
+
+void clock_setter(void) {
+    char string[81];
+    int i = 0;
+    while(1) { 
+        gettimeofday(&timeofday, NULL);
+        time_prev, time_now, start_time = (unsigned long long)(timeofday.tv_sec) * 1000 + (unsigned long long)(timeofday.tv_usec) / 1000;
+
+        while(time_now < start_time + 1000) {                   //while time between 0-1s, sample
+            gettimeofday(&timeofday, NULL);                     //constantly check time
+            time_now = (unsigned long long)(timeofday.tv_sec) * 1000 + (unsigned long long)(timeofday.tv_usec) / 1000; 
+
+            if(time_now >= time_prev + 1) {                     //if a millisecond has passed
+                sample(time_now, digitalRead(PIN), samples[i]);             //takes a snapshot
+                time_prev = time_now;                           //remember last sample time
+            }
         }
-    }
-    
-    struct Pair sample(int t, int v, struct Pair pair) {
-        pair.time = t;
-        pair.value = v;
-        return pair;
-    }
+        
 
-    system("avrdude")
+
+        sprintf(string, "sudo avrdude -C ~/avrdude_gpio.conf -c pi_1 -p atmega88p -U eeprom:w:%x,%x:m", offset, direction);
+        system(string);
+    }
+}
+
+struct Pair sample(int t, int v, struct Pair pair) {
+    pair.time = t;
+    pair.value = v;
+    return pair;
 }
