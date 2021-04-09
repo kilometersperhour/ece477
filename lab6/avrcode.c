54678e29da0a45eedb5184688f86c480ebd03051
Miles Martin <miles.martin@maine.edu>
2021-04-09 14:26:04 -0400

    Using the better sample code provided by the professor

--- lab6/avrcode.c
+++ lab6/avrcode.c
@@ -1,77 +1,103 @@
 #include <avr/io.h>
-//#include <avr/iom8.h> // why is this here, Bruce?? This should never be included directly! See line 38, <avr/iom8.h>
 #include <avr/interrupt.h>
+#include <avr/eeprom.h>
 #include <stdio.h>
+#include <math.h>
+#include <avr/sleep.h>
+#define F_CPU 8000000UL
+#include <util/delay.h>
+#include <string.h>
 
-int step_val=0;
-int compare_val=0;
-char int_counter=0;
-char increment=1;
-void init_pwm(void);
+int serial_putchar(char, FILE *);
+int serial_getchar(FILE *);
+static FILE serial_stream = FDEV_SETUP_STREAM (serial_putchar, serial_getchar, _FDEV_SETUP_RW);
+
 void init_serial(void);
+void init_adc(void);
+int read_adc(void);
 
-int main(void) {
+void update_clock_speed(void);
 
-init_pwm();
-init_serial();
 
-TIMSK=4; //TOV1
-
-sei();   //enable interrupts
-
-    while(1) {
-        while((UCSRA&(1<<RXC)) == 0);  //WAIT FOR CHAR
-        UDR = UDR+1;
-        while((UCSRA&(1<<UDRE)) == 0); //wait until empty (not necessary)
-    }   
-
-    return 0;
+int main()
+{ char buffer[100]="notStart";
+  update_clock_speed();  //adjust OSCCAL
+  init_serial(); 
+  init_adc();  
+  _delay_ms(1000); //let serial work itself out
+  while(strncmp("Start",buffer,strlen("Start"))!=0) fgets(buffer,100,stdin);
+  while(1) //raspberry pi controls reset line
+  {
+    printf("%d\n",read_adc());
+  }    
 }
 
-ISR(TIMER1_OVF_vect) {
 
-    int_counter++;
-
-    if (int_counter>1) {
-
-        int_counter=0;
-        
-	if (step_val >= 100) increment = -1;
-        if (step_val <= 0) increment = 1;
-        
-	step_val += increment;
-        
-	compare_val = step_val * step_val;
-
-        OCR1A = compare_val;
-
-    }
-
+//read the first two bytes of eeprom, if they have been programmed
+//use the first byte as an offset for adjusting OSCCAL and the second as
+//the direction to adjust 0=increase, 1=decrease.
+//Any offset is allowed and users are cautioned that it is possible to
+// adjust the oscillator beyond safe operating bounds.
+void update_clock_speed(void)
+{
+  char temp;
+  temp=eeprom_read_byte((void *)1); //read oscillator offset sign 
+                                    //0 is positive 1 is  negative
+                                    //erased reads as ff (so avoid that)
+  if(temp==0||temp==1)      //if sign is invalid, don't change oscillator
+  {
+      if(temp==0)
+          {
+             temp=eeprom_read_byte((void *)0);
+                 if(temp != 0xff) OSCCAL+=temp;
+          }
+          else
+          {
+             temp=eeprom_read_byte((void *)0);
+                 if(temp!=0xff) OSCCAL -=temp;
+          }
+  }
 }
 
-void init_pwm(void) {
+/* Initializes AVR USART for 9600 baud (assuming 8MHz clock) */
+/* 8MHz/(16*(51+1)) = 9615 about 0.2% error                  */
+void init_serial(void)
+{
+   UBRR0H=0;
+   UBRR0L=51; // 9600 BAUD FOR 1MHZ SYSTEM CLOCK
+   UCSR0A=0;
+   UCSR0C= (1<<USBS0)|(3<<UCSZ00) ;  // 8 BIT NO PARITY 2 STOP
+   UCSR0B=(1<<RXEN0)|(1<<TXEN0)  ; //ENABLE TX AND RX ALSO 8 BIT
+   stdin=&serial_stream;
+   stdout=&serial_stream;
 
-    DDRB = 2;  // make led output
-    OCR1A = 0;  // compare register is equal to zero
-
-    // Output compare OC1A 8 bit non inverted PWM
-    TCCR1A = 0xc0;  // inverted on A, nothing on B PWM mode 8
-
-    // start timer without prescaler
-    TCCR1B = 0x11; // internal clock, no prescaler , PWM mode 8
-
-    ICR1 = 10000;  // brightness levels from 0-100 control rms (steps are squares)
-
+}   
+//simplest possible putchar, waits until UDR is empty and puts character
+int serial_putchar(char val, FILE * fp)
+{
+  while((UCSR0A&(1<<UDRE0)) == 0); //wait until empty 
+   UDR0 = val;
+   return 0;
 }
 
-void init_serial(void) {
-
-    UBRRH = 0;
-
-    UBRRL = 12; // 4800 BAUD FOR 1MHZ SYSTEM CLOCK
-
-   UCSRC = (1<<URSEL) | (1<<USBS) | (3<<UCSZ0);  // 8 BIT NO PARITY 2 STOP
-
-   UCSRB = (1<<RXEN) | (1<<TXEN); //ENABLE TX AND RX ALSO 8 BIT
-
+//simplest possible getchar, waits until a char is available and reads it
+//note:1) it is a blocking read (will wait forever for a char)
+//note:2) if multiple characters come in and are not read, they will be lost
+int serial_getchar(FILE * fp)
+{
+   while((UCSR0A&(1<<RXC0)) == 0);  //WAIT FOR CHAR
+   return UDR0;
+}     
+void init_adc(void)
+{
+	ADMUX = (3<<REFS0) | 8; //temperature sensor 1.1V ref
+	ADCSRA = (1<<ADEN) | (6<<ADPS0); // enable ADC, prescaler=64
+	ADCSRB = 0;
+	DIDR0 = 0;
+} 
+int read_adc(void)
+{
+    ADCSRA |= (1<<ADSC);
+    while(ADCSRA & (1<<ADSC)); //wait for coversion
+    return ADC;
 }
