06a08c5504c1a90279b5b7dfed507657d8143c5c
Miles Martin <miles.martin@maine.edu>
2021-04-08 21:32:01 -0400

    Some thoughts/contributions to lab3, since this will never get looked at anyway

--- lab3/main.c
+++ lab3/main.c
@@ -12,39 +12,39 @@
 
 #define HOLD_STATE 1000  // Hold the LEDs at a certain state for this many millis
 
-void pwm(float duty_cycle_pct[], float period_ms, int pins[]);
+void pwm(float duty_cycle_pct[], float period_ms, const int pins[]);
 
-const char args_error_string[] = "Error. Arguments passed. Do not pass arguments.";
+const char args_error_string[] = "Error. Arguments passed. Do not pass arguments.\n";
 
-const int pin_row[8] = { // map GPIO-7 to corresponding wiringPi pins
-30,   // GPIO 0
-31,   // GPIO 1
-8,    // GPIO 2
-9,    // GPIO 3
-7,    // GPIO 4
-21,   // GPIO 5
-22,   // GPIO 6
-11    // GPIO 7
+const int pin_row[8] = { // map GPIO to corresponding wiringPi pins 0-7
+0,  // GPIO 17
+1,  // GPIO 18
+2,  // GPIO 27
+3,  // GPIO 22
+4,  // GPIO 23
+5,  // GPIO 24
+6,  // GPIO 25
+7   // GPIO 4
 };
 
 const int num_pins = sizeof(pin_row)/sizeof(pin_row[0]);  // Max times to loop
 
 const float divs_array[8] = { // an array of divisors
-4./255,  // Where each increasing LED must light up when the load doubles,
-8./255,  // each value of this array doubles in kind.
-16./255,  // The values have been multiplied by a factor of four
-32./255,  // because the max load is 4/4.
-64./255, // The values are divided by 255 to normalize them
-128./255, // 255 is the sum of 2^0 through 2^7, representing the
-256./255, // corresponding weights of each LED's brightness.
-512./255
+4./128,  // Where each increasing LED must light up when the load doubles,
+4./64,   // each value of this array doubles in kind.
+4./32,   // The values have been multiplied by a factor of four
+4./16,   // because the max load is 4/4.
+4./8,    // The values are divided by 255 to normalize them
+4./4,    
+4./2,    
+4.
 };
 
 int main (int argc, char *argv[]) {
 	
 	if (argc != 1) {
 		printf("%s", args_error_string);
-		return 1;
+		return -1;
 	} 
 	
 	char string_buffer[4];
@@ -65,81 +65,52 @@ int main (int argc, char *argv[]) {
 		open_file_fd = open("/proc/loadavg",O_RDONLY);
 		read(open_file_fd, string_buffer, 4);
 		stress = strtof(string_buffer, NULL); // string to float()
-//		printf("%f is stress\n",stress);
 		
-		for(i = 0; i < 8; i++) {
-			if(i == 0) {
-				// Takes input to calculate first value in load_dif
-				load_diff[0] = stress - divs_array[0];        
+		// 4/5/2021
+		for (i = 0; i < 8; i++) {
+			if (stress > divs_array[i]) {
+				load_diff[i] = 1;
+			} else if (stress > 0) {
+				load_diff[i] = stress/divs_array[i];
+				while (i < 8-1) {
+					load_diff[i+1] = 0;
+				}
 			}
-			else {
-				// Uses previous load_dif value to calculate current value
-            			load_diff[i] = load_diff[i - 1] - divs_array[i];           
-			}
+			printf("%f, ", load_diff[7-i]);
 		}
-		
-		for(i=0; i<8; i++) {
-	//		printf("%f \n", load_diff[i]);
-		}
-		
-		// printf("I made it out!!\n");
+		printf("[7..0]\n");
 
-		// if load is greater than 1, and next is not, 
-		// then set that last pin to be equal to the percent brightness of the load
-		// send it a single pin pin_row[i]
-
-		for(int i = 0; i < 8; i++) {			// Runs once per pin (8 pins)
-			// If the factor_of_divs is negative, 
-			// grab the last pins factor 
-			// and set that brightness to the pin
-	//		printf("Doing WiringPi pin %d\n", pin_row[i]);
-//			if ((divs_array[i] > load_diff[i]) && (i == 0))  { 	
-	
-			factor_of_divs[i] = load_diff[i]/divs_array[i];
-
-			if (factor_of_divs[i] > 1) {
-				factor_of_divs[i] = 1;
-			}
-			else if (factor_of_divs[i] < 0) {
-				factor_of_divs[i] = 0;
-			}
-
-		}
-	
-		for(i=0; i<8; i++) {
-	//		printf("%f \n", factor_of_divs[i]);
-		}
-
-		pwm(factor_of_divs, 20, pin_row);
-		delay(2000);
+		pwm(load_diff, 20, pin_row);
 	}	
 }
 
-void pwm(float duty_cycle_pct[], float period_ms, int pins[]) {
+void pwm(float duty_cycle_pct[], float period_ms, const int pins[]) {
 		
 	float time_high;
-	
-	int i,j;
+	int dim_led = 7; // the LED we will perform PWM on
+	int i;
 //	int num_pins = sizeof(pins)/sizeof(pins[0]);
 //	printf("%d is num_pins",num_pins);
 //	for (j = 0; j < 500; j++) {
-		for (i = 0; i < 8; i++) {
-			if (duty_cycle_pct[i] > 0) {
-				digitalWrite(pins[i], HIGH);  // set pins; turn on lights
-			}
-	
-			if ((duty_cycle_pct[i] != 1) && (duty_cycle_pct[i] != 0)) {
-				time_high = (duty_cycle_pct[i]) * (period_ms);
-				delay(time_high);  // keep high appropriately long
-			}
-			
-			if (duty_cycle_pct[i] < 1) {
-				digitalWrite(pins[i], LOW);  // clear pins; turn off lights
-			}
-	
-			if ((duty_cycle_pct[i] != 1) && (duty_cycle_pct[i] != 0)) {
-				delay(period_ms - time_high);  // keep dark appropriately long
-			}
+	for (i = 0; i < 8; i++) {
+		if (duty_cycle_pct[i] == 1) {
+			digitalWrite(pins[i], HIGH);  // set pins; turn on lights
+		} else if (duty_cycle_pct[i] == 0 ) {
+			digitalWrite(pins[i], LOW);  // clear pins; turn off lights
+		} else {
+			dim_led = i;
 		}
+	}
+
+	time_high = (duty_cycle_pct[dim_led]) * (period_ms);
+	
+	for (i = 0; i < 50; i++) {
+
+		digitalWrite(pins[dim_led], HIGH);  // turn on LED
+		delay(time_high);  // keep lit appropriately long
+		
+		digitalWrite(pins[dim_led], LOW);  // turn off LED
+		delay(period_ms - time_high);  // keep dark appropriately long
+	}
 //	}
 }
--- lab3/something_cool.c
+++ lab3/something_cool.c
@@ -7,37 +7,38 @@
 
 int main(void) {
 
-    const int pin_row[] = { // map GPIO-7 to corresponding wiringPi pins
-        30,   // GPIO 0
-        31,   // GPIO 1
-        8,    // GPIO 2
-        9,    // GPIO 3
-        7,    // GPIO 4
-        21,   // GPIO 5
-        22,   // GPIO 6
-        11    // GPIO 7
-    };
-    float stress[] = {1.24, 3.62, 1.98, 0.79, 3.26, 1.23, 1.89, 0.32, 3.82, 4.0};  //Fake values to test
-    float history[8] = {0, 0, 0, 0, 0, 0, 0, 0}; 
-    int i = 0;
+    
+	const int pin_row[8] = { // map GPIO-7 to corresponding wiringPi pins
+	0,  // GPIO 17
+	1,  // GPIO 18
+	2,  // GPIO 27
+	3,  // GPIO 22
+	4,  // GPIO 23
+	5,  // GPIO 24
+	6,  // GPIO 25
+	7   // GPIO 4
+	};
+    
+	float stress[] = {1.24, 3.62, 1.98, 0.79, 3.26, 1.23, 1.89, 0.32, 3.82, 4.0};  //Fake values to test
+	float history[8] = {0, 0, 0, 0, 0, 0, 0, 0}; 
+	int i = 0;
 
-    while(1) { 
-//        for(int i = 6; i >= 0; i--) {
-//            pwm(history[i], 1, pin_row[i]);    
-//        }
-        history[7] = stress[i] / 4.;          //Manually set last history index with new stress value
-        pwm(history, 1, pin_row);             //Turn on LEDs based on history values
+	while(1) { 
+        	history[7] = stress[i] / 4.;          //Manually set last history index with new stress value
+//        	pwm(history, 1, pin_row);             //Turn on LEDs based on history values
 
-        for(int j = 0; j < 6; j++) {          //Update history array
-            history[j] = history[j+1];        //By shifting all values down one
-        }
+        	for(int j = 0; j < 6; j++) {          //Update history array
+        		history[j] = history[j+1];        //By shifting all values down one
+        	}
         
-        i++;                                  //Testing fake values
-        if(i > 9) i = 0;
+		i++;                                  //Testing fake values
+        
+		if (i > 9) i = 0;
 
-        for(int x = 0; x < 8; x++) {          //Print History array values
+        for (int x = 0; x < 8; x++) {          //Print History array values
             printf("%f", history[x]);
         }
+
         delay(5000);
     }
 }
