59731a7a1a3b65ac42ffa62fa952abcb8a4f9aa2
Jesse Perkins <jnperkins13@gmail.com>
2021-04-30 16:46:50 -0400

    Tweaked youDied function, added comments

--- final/RoundSetup.c
+++ final/RoundSetup.c
@@ -4,26 +4,23 @@
 
 void roundSetup(int reset) {
     if (reset == 1) {
-        static uint16_t state = 0;
+        static uint16_t state = 0; // If reset was pressed, set state equal to 0
     }
     char serialBuffer[15] = {0};
 
     for (int i = 0; i < 15; i++) {
-        if ((state >> i) & 1 == 0) { // only compare i-th bit of state for any i
-            sprintf(serialBuffer, "atc0=(%d,5,5,5)", i); // white pixel command string
+        if ((state >> i) & 1 == 0) { // if i-th bit of state is 0, 
+            sprintf(serialBuffer, "atc0=(%d,5,5,5)", i); // set i-th pixel as white
         } else {
-            sprintf(serialBuffer, "atc0=(%d,5,0,0)", i); // red pixel command string
+            sprintf(serialBuffer, "atc0=(%d,5,0,0)", i); // else set i-th pixel as red
         }
 	serialChatter(serialBuffer) // send off formatted string with serialPuts
     }
 }
 
 extern void youDied() {
-    serialWrite(fd, "at10=(5,0,0,4)");
-    serialWrite(fd, "at10=(0,0,0,8)");
-    for(int i = 0; i < 15; i++) {
-        state[i] = 0;
-    }
+    serialWrite(fd, "at10=(5,0,0,4)"); // Play red death animation
+    serialWrite(fd, "at10=(0,0,0,8)"); // Fade to black
 }
 
 void serialChatter(int fildes, char string) {
