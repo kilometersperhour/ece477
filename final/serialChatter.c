bd86dc536f9617474810d98ff52b8177c7d35c5e
Miles Martin <miles.martin@maine.edu>
2021-04-30 17:16:13 -0400

    Continuing to develop serial routines

---
+++ final/serialChatter.c
@@ -0,0 +1,40 @@
+#include <wiringPi.h>
+#include <wiringSerial.h>
+#include <stdlib.h>
+
+//GPIO Pins 20 and 23 for reset and play
+#define PIN_RESET 20
+#define PIN_GAME 23
+
+int deviceSetup(){
+	wiringPiSetup();
+	// Setup inputs for the button boys
+	for(int i; i <= NUM_BUTTONS; i++){
+		pinMode(button[i], INPUT);
+	}
+	serialPortInit(); // Serial port setup
+	wiringPiISR(PIN_RESET, INT_EDGE_RISING, &letgo_reset); // interrupt handlers
+	wiringPiISR(PIN_GAME, INT_EDGE_RISING, &letgo_game);
+}
+
+int main() {
+
+	serialChatter
+
+}
+
+void serialChatter(char string) {
+
+	printf("Sending \'%s\'...", string);
+	
+	while (serialDataAvail(fd))
+	{
+		serialGetchar(fd);
+		fflush (stdout) ;
+	}
+	
+	serialPuts(fd, string);
+
+	printf("Received \'%s\'in response.");
+
+}
