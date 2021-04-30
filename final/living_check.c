21b3fb9fde475c39f16bcb952384eecd9272b220
Miles Martin <miles.martin@maine.edu>
2021-04-30 17:21:30 -0400

    added comments to living_check.c

--- final/living_check.c
+++ final/living_check.c
@@ -1,10 +1,10 @@
 void livingCheck(){
 	// Takes input from main.c and checks if you landed on red or white pixel
-	int life_status = 1;
+	int life_status = 1; // alive.
 
 	// Landed on a red pixel, change life_status to zero and end game
 	if((state | green) == state){
-		life_status = 0;
+		life_status = 0; // dead.
 		return 0;
 	}
 	// Landed on a white pixel, exit function 
