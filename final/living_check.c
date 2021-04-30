3d4f0c27e8ae8821cba52e1ebd203a50372404cb
Travis John Nickerson <travis.nickerson@maine.edu>
2021-04-30 21:48:22 +0100

    Fixed living_check function to be operation with bitwise, removed loops that were obselete

--- final/living_check.c
+++ final/living_check.c
@@ -1,20 +1,12 @@
-livingCheck(){
-	// Look mom, Im alive
+void livingCheck(){
+	// Takes input from main.c and checks if you landed on red or white pixel
 	int life_status = 1;
-	int i;
-	last_cursor_loc = //TODO Serial for current location
-	//TODO NEED state to be defined
 
-	Do{
-		if(state[i] == last_cursor_loc){
-			// You have died and so has my faith in you
-			life_status = 0;
-			return 0;
-		}
-		i++;
+	// Landed on a red pixel, change life_status to zero and end game
+	if((state | green) == state){
+		life_status = 0;
+		return 0;
 	}
-	// Check if you still have a will to live
-	while((life_status == 1) && (state[i] != 16)){
-		return 1;
-	}
+	// Landed on a white pixel, exit function 
+	return 1;
 }
