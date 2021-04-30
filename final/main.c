1379fffcfc1a18e4f150d86adef18467978f3665
Travis John Nickerson <travis.nickerson@maine.edu>
2021-04-30 03:46:37 +0100

    Added some headers to main.c

--- final/main.c
+++ final/main.c
@@ -1,7 +1,15 @@
 //////////////////////////////////////////////////////////////////
 //								//
-//		"Knockout Color Ring Game"			//
+//		"Knockout Game (Bidirectional)"			//
 //	Final Project for ECE 477 - Spring 2021 UMaine		//
 //		by Miles M, Jesse P, Travis N			//	
 //								//
 //////////////////////////////////////////////////////////////////
+
+#include <wiringPi.h>
+#include <wiringSerial.h>
+#include <stdlib.h>
+
+int main(){
+
+}
