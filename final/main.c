53915e84484afdf3d79035cf17e6921e426beb3c
Travis John Nickerson <travis.nickerson@maine.edu>
2021-04-30 21:03:18 +0100

    Added interrupts.c to main.c

--- final/main.c
+++ final/main.c
@@ -31,3 +31,22 @@ int main(){
 	
 
 }
+
+letgo_reset() {
+	static int last_reset_press = 0; // debounce
+	if ((last_reset_press + 32) >= millis()) { // valid press detected; not bounce
+		roundSetup(1);
+	}
+	last_reset_press = millis();
+}
+
+letgo_play() {
+    	static int last_play_press = 0; // debounce
+	if ((last_play_press + 32) >= millis()) { // valid press detected; not bounce
+		// save new status of cursor
+		livingCheck(); //
+		direction *= -1; // change direction of gameplay
+	}
+	last_play_press = millis();
+
+}
