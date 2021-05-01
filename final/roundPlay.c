47a5fb2e9baafa067ef36808db023da0d49f7bfd
Miles Martin <miles.martin@maine.edu>
2021-04-30 22:43:48 -0400

    Changed 'green' to 'cursor' and I think that's it

--- final/roundPlay.c
+++ final/roundPlay.c
@@ -1,27 +1,28 @@
 #include <stdint.h> // for _t types
 
-#DEFINE MS 1000000000000000
-#DEFINE LS 0000000000000001
+#define MS (1<<15)
+#define LS 1
 
-...
+void roundPlay() {
 
-static uint16_t green = 1;
+	static uint16_t cursor = 1;
 
-static int direction = 1;
+	static int direction = 1;
 
-while(livingCheck()) {
-	if(0 < direction) {
-		//if the direction is 1 (going right to left) and 
-		//the current state isn't LS, shift left LED by 
-		//one. If it is LS, wrap around to MS
-	       	green = (green == MS)? LS: green << 1; 
+	while(livingCheck()) {
+		if(0 < direction) {
+			//if the direction is 1 (going right to left) and 
+			//the current state isn't LS, shift left LED by 
+			//one. If it is LS, wrap around to MS
+		       	cursor = (cursor == MS)? LS: cursor << 1; 
+		}
+		else {
+			//if the direction is not 1 (going left to right)  
+			//and the current state isn't RS, shift right LED by 
+			//one. If it is MS, wrap around to LS
+			cursor = (cursor == LS)? MS: cursor >> 1;
+		}
 	}
-	else {
-		//if the direction is not 1 (going left to right)  
-		//and the current state isn't RS, shift right LED by 
-		//one. If it is MS, wrap around to LS
-		green = (green == LS)? MS: green >> 1;
-	}
+
+	return;
 }
-
-return 0;
