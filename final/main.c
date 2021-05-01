4c640c744f84347446899ca6f44c94bcc5d08f69
Miles Martin <miles.martin@maine.edu>
2021-04-30 22:29:03 -0400

    addded livingCheck to main.c

--- final/main.c
+++ final/main.c
@@ -97,6 +97,21 @@ letgo_play() {
 
 }
 
+int livingCheck(){
+	// Takes input from main.c and checks if you landed on red or white pixel
+	int life_status = 1; // alive.
+
+	// Landed on a red pixel, change life_status to zero and end game
+	if((state | green) == state){
+		life_status = 0; // dead.
+		return 0;
+	}
+	// Landed on a white pixel, exit function 
+	return 1;
+}
+
+
+
 void serialChatter(int fd, char * string, int wait_ms) {
 
 //	int wait_until = millis() + wait_ms;
